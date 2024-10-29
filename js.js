8792614c6762b5aa2f5368dae6867cf223bc5a44
MadelineSIPAML <162452139+MadelineSIPAML@users.noreply.github.com>
2024-10-29 08:42:00 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,17 @@
+var nombre = document.querySelector("#nombre")
+
+
+function alerta(){
+
+    alert("Hola soy Madeline Toledo")
+}
+
+
+function cambiarColor(){
+
+    nombre.style.color=" springgreen"
+}
+
+function cambiarColor2(){
+    nombre.style.color="aqua"
+}
